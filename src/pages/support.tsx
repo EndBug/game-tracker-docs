37fa2bf66b057ce02e7df58cc8ab53242643e90e
Federico Grandi <fgrandi30@gmail.com>
2022-06-27 20:29:15 +0000

    chore: move to Vercel

--- src/pages/support.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import RedirectPage from '../components/RedirectPage'
-
-export default function Support() {
-  return <RedirectPage href="https://discord.gg/5YrhW4NHfY" />
-}
