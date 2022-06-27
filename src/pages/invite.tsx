cecb8fa9b9f3f913b7d82f14a9daf2430a902fa8
Federico Grandi <fgrandi30@gmail.com>
2022-06-27 21:14:48 +0000

    chore: dynamically update fallback redirects based on vercel config

--- src/pages/invite.tsx
+++ src/pages/invite.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import RedirectPage from '../components/RedirectPage'
+import vercel from '../../vercel.json'
 
 // This is jsut a fallback, there is a server-side redirect set up in /vercel.json
 
 export default function Support() {
   return (
-    <RedirectPage href="https://discord.com/api/oauth2/authorize?client_id=475421235950518292&permissions=16384&scope=bot%20applications.commands" />
+    <RedirectPage href={vercel.redirects.find(e => e.source == '/invite')?.destination} />
   )
 }
--- src/pages/support.tsx
+++ src/pages/support.tsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import RedirectPage from '../components/RedirectPage'
+import vercel from '../../vercel.json'
 
 // This is jsut a fallback, there is a server-side redirect set up in /vercel.json
 
 export default function Support() {
-  return <RedirectPage href="https://discord.gg/5YrhW4NHfY" />
+  return <RedirectPage href={vercel.redirects.find(e => e.source == '/support')?.destination} />
 }
